6d660d83b47517b2df7c44ad463bed4a5b509466
azez-alhoot <azezalhoot82@yahoo.com>
2020-08-20 18:33:05 +0300

    fixing errors

--- data_structures_and_algorithms_python/challenges/array_shift/array_shift.py
+++ data_structures_and_algorithms_python/challenges/array_shift/array_shift.py
@@ -3,5 +3,5 @@ def insertShiftArray(lst,n):
     if len(lst) %2 == 0:
         return lst[:len(lst)//2] + [n] + lst[len(lst)//2:]
     else:
-        midpoint = len(lst)//2
+        midpoint = (len(lst)//2) + 1
         return lst[0:midpoint] + [n] + lst[midpoint:]
